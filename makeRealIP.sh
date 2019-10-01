05475741b54fe95057ff441ec0c87d8b448fd804
Alivekeep <2fed.tcp@gmail.com>
2019-10-01 12:32:31 +0300

    refactor(install script, deps):

--- makeRealIP.sh
+++ makeRealIP.sh
@@ -1,4 +1,4 @@
 IP=$( /sbin/ip route|awk '/default/ { print $3 }');
 
-sed -i "4s/127.0.0.1/${IP}/g" ./server/nats-settings.js
-sed -i "5s/127.0.0.1/${IP}/g" ./server/nats-settings.js
+sed -i "4s/127.0.0.1/${IP}/g" ./server/settings.js
+sed -i "5s/127.0.0.1/${IP}/g" ./server/settings.js
