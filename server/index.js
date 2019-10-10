ca3c7b38c699dd7b57e5e96e34b28e91dcb9fcc1
Alivekeep <2fed.tcp@gmail.com>
2019-10-10 11:40:28 +0300

    refactor(server):
    removed express

--- server/index.js
+++ server/index.js
@@ -1,24 +1,22 @@
-const { Server } = require("http");
-const express = require("express");
-const socketio = require("socket.io");
-const { join } = require("path");
+const { createServer } = require("http");
+const Socketio = require("socket.io");
+const serveStatic = require("serve-static");
+const finalhandler = require("finalhandler");
 
 const { handler } = require("./events/sockets_handlers");
 
-const app = express();
-const http = Server(app);
+const serve = serveStatic("build", { index: ["index.html"] });
 
-app.use(express.static("build"));
-
-const io = socketio(http);
-io.on("connection", handler);
-
-app.get("*", (req, res) => {
-  res.sendFile(join(`${__dirname}/../build/index.html`));
+const app = createServer(async (req, res) => {
+  serve(req, res, finalhandler(req, res));
 });
 
-const server = http.listen(8282, () => {
+const io = new Socketio(app);
+
+io.on("connection", handler);
+
+app.listen(8282, () => {
   console.log(
-    `Server app was started at http://127.0.0.1:${server.address().port}`
+    `Server app was started at http://127.0.0.1:${app.address().port}`
   );
 });
