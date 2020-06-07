f7ffb63ad5d9997af18cc3e2b0e046434ca214f1
sindbad911 <sindbad911@bk.ru>
2020-06-08 00:26:30 +0300

    refactor(app, react):
    updated CORS
    updated ignore files

--- server/index.js
+++ server/index.js
@@ -8,15 +8,25 @@ const { handler } = require("./events/sockets_handlers");
 const serve = serveStatic("build", { index: ["index.html"] });
 
 const app = createServer(async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
   serve(req, res, finalhandler(req, res));
 });
-
-const io = new Socketio(app);
-
-io.on("connection", handler);
 
 app.listen(8282, () => {
   console.log(
     `Server app was started at http://127.0.0.1:${app.address().port}`
   );
 });
+
+const io = new Socketio(app);
+
+io.on("connection", handler);
